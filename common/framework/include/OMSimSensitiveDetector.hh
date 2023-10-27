a2be18fad2c13fa1865f773201afc661b493059e
martinunland <martin.e@unland.eu>
2023-10-27 14:40:52 +0200

    added boundary detection for future photocathodes

--- common/framework/include/OMSimSensitiveDetector.hh
+++ common/framework/include/OMSimSensitiveDetector.hh
@@ -4,7 +4,7 @@
 #include "G4VSensitiveDetector.hh"
 #include "G4ThreeVector.hh"
 #include "OMSimPMTResponse.hh"
-
+#include "G4OpBoundaryProcess.hh"
 #include <vector>
 
 class G4Step;
@@ -18,7 +18,8 @@ class G4TouchableHistory;
  */
 enum class DetectorType {
     PMT,                           ///< Photomultiplier tube detector.
-    GeneralPhotonDetector          ///< General photon detector.
+    VolumePhotonDetector,          ///< Photon detector based on absorption in volume.
+    BoundaryPhotonDetector         ///< Photon detector based on absorption in boundary.
 };
 
 /**
@@ -58,11 +59,15 @@ public:
 private:
     OMSimPMTResponse *mPMTResponse;
     DetectorType mDetectorType;
-    G4bool checkAbsorbedPhotons(G4Step *pStep);
+    G4OpBoundaryProcess* mBoundaryProcess = nullptr;
+
+    G4bool checkVolumeAbsorption(G4Step *pStep);
+    G4bool checkBoundaryAbsorption(G4Step *pStep);
     PhotonInfo getPhotonInfo(G4Step *pStep);
     G4bool handlePMT(G4Step *pStep, G4TouchableHistory *pTouchableHistory);
     G4bool handleGeneralPhotonDetector(G4Step *pStep, G4TouchableHistory *pTouchableHistory);
     void storePhotonHit(PhotonInfo &info);
+    void fetchBoundaryProcess();
 };
 
 #endif
