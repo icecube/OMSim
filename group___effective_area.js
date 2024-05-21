61e7f7e9daaf1760642d05405867260ae4d8959b
martinunland <martinunland@users.noreply.github.com>
2024-05-21 15:47:45 +0000

    deploy: 9205aa8dfb82b7dd945699a399c754f634268e1b

--- group___effective_area.js
+++ group___effective_area.js
@@ -32,8 +32,8 @@ var group___effective_area =
       [ "OMSimEffectiveAreaAnalyisis", "class_o_m_sim_effective_area_analyisis.html#abed7b6d51b06cc75d4110db740734776", null ],
       [ "~OMSimEffectiveAreaAnalyisis", "class_o_m_sim_effective_area_analyisis.html#a64b63b8e79e9d2100d951a4e231c2f75", null ],
       [ "calculateEffectiveArea", "class_o_m_sim_effective_area_analyisis.html#aa9380b8148a1a0846b2f026d7dfa0046", null ],
-      [ "writeHeader", "class_o_m_sim_effective_area_analyisis.html#a8d49aa6cccb6e98ebb9a084284ce47b9", null ],
-      [ "writeScan", "class_o_m_sim_effective_area_analyisis.html#ad9b76bc60e5f3629a9c693c2a30f539d", null ],
+      [ "writeHeader", "class_o_m_sim_effective_area_analyisis.html#a400b1a20fbf322894cd7c2564a7dbb35", null ],
+      [ "writeScan", "class_o_m_sim_effective_area_analyisis.html#af2f7ac6a789bb50ec7d0d8a81487ec20", null ],
       [ "mDatafile", "class_o_m_sim_effective_area_analyisis.html#ac392f952bcc7e92c303e8e371b00c594", null ],
       [ "mOutputFileName", "class_o_m_sim_effective_area_analyisis.html#a93a35ab4f557df790de84ec0ab5e9fab", null ]
     ] ],
